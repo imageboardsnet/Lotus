1b33e4abef640cbd2a666bc4c5d50f4a1b390127
me <me@imageboards.net>
2024-02-02 01:50:55 +0100

    add favicon add new search feature

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import json
 import schedule
 import time
 import threading
+import mimetypes
+from urllib.parse import urlparse
+
 
 app = Flask(__name__)
 
@@ -13,23 +16,67 @@ boards_json_url = "https://imageboardsnet.github.io/imageboards.json/imageboards
 def get_imageboards():
     response = requests.get(boards_json_url)
     imageboards = json.loads(response.text)
+    for imageboard in imageboards:
+        if 'url' in imageboard:
+            parsed_url = urlparse(imageboard['url'])
+            imageboard['cleanurl'] = parsed_url.netloc
     return imageboards
 
-def render_template_outside_of_view():
+def render_boards():
     with app.app_context():
         rendered_template = render_template('boards.html', imageboards=imageboards)
+        return rendered_template
+    
+def available_languages():
+    languages = []
+    for imageboard in imageboards:
+        if 'language' in imageboard:
+            if isinstance(imageboard['language'], list):
+                for lang in imageboard['language']:
+                    if lang not in languages:
+                        languages.append(lang)
+            else:
+                lang = imageboard['language']
+                if lang not in languages:
+                    languages.append(lang)
+    return languages
+
+def available_software():
+    software = []
+    for imageboard in imageboards:
+        if 'software' in imageboard:
+            if isinstance(imageboard['software'], list):
+                for soft in imageboard['software']:
+                    if soft not in software:
+                        software.append(soft)
+            else:
+                soft = imageboard['software']
+                if soft not in software:
+                    software.append(soft)
+    return software
+
+def render_search():
+    with app.app_context():
+        rendered_template = render_template('search.html', languages=languages, softwares=software)
         return rendered_template
 
 def update_imageboards():
     global imageboards
     imageboards = get_imageboards()
+    global languages
+    languages = available_languages()
+    global software
+    software = available_software()
 
-def update_imageboards_prerendered():
+def update_rendered():
     global imageboards_prerendered
-    imageboards_prerendered = render_template_outside_of_view()
+    imageboards_prerendered = render_boards()
+    global search_prerendered
+    search_prerendered = render_search()
+
 
 schedule.every(30).minutes.do(update_imageboards)
-schedule.every(30).minutes.do(update_imageboards_prerendered)
+schedule.every(30).minutes.do(update_rendered)
 
 def schedule_run():
     while True:
@@ -38,8 +85,8 @@ def schedule_run():
 
 @app.route('/')
 def home():
-    search_content = render_template('search.html')
-    return render_template('index.html', content=search_content + imageboards_prerendered, nav="1")
+    
+    return render_template('index.html', content= search_prerendered + imageboards_prerendered, nav="1")
 
 @app.route('/myboard')
 def myboard():
@@ -58,14 +105,14 @@ def favicon():
 
 if __name__ == '__main__':
     update_imageboards()
-    update_imageboards_prerendered()
+    update_rendered()
     schedule_thread = threading.Thread(target=schedule_run)
     schedule_thread.start()
     app.run() 
 
 def create_app():
     update_imageboards()
-    update_imageboards_prerendered()
+    update_rendered()
     schedule_thread = threading.Thread(target=schedule_run)
     schedule_thread.start()
     return app
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     var options = {
-        valueNames: ['name', 'language','software']
+        valueNames: ['name', 'language', 'software']
     };
 
-    var userList = new List('boards', options);
+    var featureList = new List('boards', options);
+
+    document.getElementById('language').addEventListener('change', function () {
+        var selection = this.value;
+        if (selection) {
+            featureList.filter(function(item) {
+                return (item.values().language === selection);
+            });
+        } else {
+            featureList.filter();
+        }
+    });
+
+    document.getElementById('software').addEventListener('change', function () {
+        var selection = this.value;
+        if (selection) {
+            featureList.filter(function(item) {
+                return (item.values().software === selection);
+            });
+        } else {
+            featureList.filter();
+        }
+    });
 
 });
+
