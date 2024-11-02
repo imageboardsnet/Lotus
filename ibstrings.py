0d05514aff78b21438e50641678512398be44010
me <me@imageboards.net>
2024-11-03 00:34:29 +0100

    optimize titles and descriptions

--- ibstrings.py
+++ ibstrings.py
@@ -1,9 +1,9 @@
-main_title = "ImageBoards.net - The largest list of Imageboards"
+main_title = "ImageBoards.net - The largest list of Imageboard"
 nothing_title = "No Results Found"
-search_title = "Search Imageboards"
+search_title = "Search Imageboard"
 about_title = "About ImageBoards.net"
 
-main_description = "The most complete and updated list of imageboards. Explore Over 200 Curated Boards"
+main_description = "The most complete and updated list of imageboard. Explore Over 200 Curated Boards"
 nothing_description = "Oops! It seems we couldn't find what you were looking for. Please try searching again or explore our curated list."
 search_description = "Easily search through our extensive database to find the perfect imageboard for your interests."
-about_description = "About Us: We are dedicated to connecting you with the vast world of imageboards. Discover how we source, organize, and update our expansive list to keep it fresh and relevant."
+about_description = "About Us: We are dedicated to connecting you with the vast world of imageboard. Discover how we source, organize, and update our expansive list to keep it fresh and relevant."
